0243937afb23235d29d993b54f1d1aea0d3e29ee
İbrahim Diyar Oskan <ioskan@stu.khas.edu.tr>
2023-09-11 12:22:36 +0300

    bişiler deniyoz

--- PythonLearning.py
+++ PythonLearning.py
@@ -800,10 +800,9 @@ import pyqrcode
 url = input("enter url to generate qr code: ")
 
 qr_code = pyqrcode.create(url)
-qr_code.svg('qrcode.svg',scale=5)
+qr_code.svg('qrcode.svg',scale= 5)
 """
-
-"""
+"silinmek üzere yazılmak"
 
 
 
