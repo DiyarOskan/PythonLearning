68bdd816f1c2a0a534a32272ae591dbec94dc791
İbrahim Diyar Oskan <ioskan@stu.khas.edu.tr>
2023-09-15 20:09:15 +0300

    en güncel hali

--- Tkinter.py
+++ Tkinter.py
@@ -88,13 +88,13 @@ my_listbox.pack()
 
 window.mainloop()
 """
-
+"""
 import tkinter
 
 window = tkinter.Tk()
 window.title("BMI Calculator")
 window.configure(bg="#DEB887")
-window.minsize(width=200,height=200)
+window.minsize(width=500,height=500)
 
 #label_1
 my_label1 = tkinter.Label(text="Enter your height(cm) ", font=("Times New Roman", 10, "italic"), bg="#DEB887", fg="#800000", padx=5, pady=5)
@@ -147,4 +147,106 @@ my_label3 = tkinter.Label(text="Fill the blanks to calculate your BMI value", fo
 my_label3.pack()
 
 
+window.mainloop()
+"""
+
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+import base64
+import os
+
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+#save notes
+def save_and_encrypt_notes():
+    title = title_entry.get()
+    message = input_text.get("1.0",END)
+    master_secret = master_secret_input.get()
+
+    if len(title) == 0 or len(message) == 0 or len(master_secret) == 0:
+            messagebox.showinfo(title="Error!", message="Please enter all information.")
+    else:
+        message_encrypted = encode(master_secret, message)
+
+        try:
+            with open("mysecret.txt", "a") as data_file:
+                data_file.write(f'\n{title}\n{message_encrypted}')
+        except FileNotFoundError:
+            with open("mysecret.txt", "w") as data_file:
+                data_file.write(f'\n{title}\n{message_encrypted}')
+        finally:
+            title_entry.delete(0, END)
+            master_secret_input.delete(0, END)
+            input_text.delete("1.0",END)
+
+#decrypt notes
+
+def decrypt_notes():
+    message_encrypted = input_text.get("1.0", END)
+    master_secret = master_secret_input.get()
+
+    if len(message_encrypted) == 0 or len(master_secret) == 0:
+        messagebox.showinfo(title="Error!", message="Please enter all information.")
+    else:
+        try:
+            decrypted_message = decode(master_secret,message_encrypted)
+            input_text.delete("1.0", END)
+            input_text.insert("1.0", decrypted_message)
+        except:
+            messagebox.showinfo(title="Error!", message="Please make sure of encrypted info.")
+
+#UI
+
+window = Tk()
+window.title("Secret File")
+window.configure(bg="#DEB887")
+window.minsize(width=300,height=500)
+
+img = Image.open("output.png")
+resize_image = img.resize((100,100))
+img = ImageTk.PhotoImage(resize_image)
+
+panel =Label(window, image = img,bg="#DEB887",fg="#DEB887",)
+panel.pack()
+
+title_info_label = Label(text="Enter your title",font=("Times New Roman", 8, "bold"), bg="#DEB887", fg="#800000", padx=5, pady=5)
+title_info_label.pack()
+
+title_entry = Entry(width=20 , bg="#FFDEAD",font=("Times New Roman", 8, "italic"), fg="#800000")
+title_entry.pack()
+
+input_info_label = Label(text="Enter your secret",font=("Times New Roman", 8, "bold"), bg="#DEB887", fg="#800000", padx=5, pady=5)
+input_info_label.pack()
+
+input_text = Text(width=25, height=15,font=("Times New Roman", 8, "normal"),bg="#FFDEAD",fg="#800000")
+input_text.pack()
+
+master_secret_label = Label(text="Enter master key",font=("Times New Roman", 8, "bold"), bg="#DEB887", fg="#800000", padx=5, pady=5)
+master_secret_label.pack()
+
+master_secret_input = Entry(width=30,font=("Times New Roman", 8, "normal"),bg="#FFDEAD",fg="#800000")
+master_secret_input.pack()
+
+save_button = Button(text="Save & Encrypt", command=save_and_encrypt_notes,font=("Times New Roman", 8, "bold"),bg="#DEB887", fg="#800000")
+save_button.pack()
+
+decrypt_button = Button(text="Decrypt",command=decrypt_notes,font=("Times New Roman", 8, "bold"),bg="#DEB887", fg="#800000")
+decrypt_button.pack()
 window.mainloop()
