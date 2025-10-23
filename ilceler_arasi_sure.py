ee3ad2414e58216eb11584a8c9e8acccf7b985e4
DiyarOskan <142227026+DiyarOskan@users.noreply.github.com>
2025-10-23 14:12:27 +0300

    Update ilceler_arasi_sure.py

--- ilceler_arasi_sure.py
+++ ilceler_arasi_sure.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import time
 import csv
 
-gmaps = googlemaps.Client(key='AIzaSyChvvOJO7XPpevVBLdpikWevUbhLA0wF3Q')
+gmaps = googlemaps.Client(key='google key')
 
 districts = [
     "Adalar", "Arnavutköy", "Ataşehir", "Avcılar", "Bağcılar", "Bahçelievler",
